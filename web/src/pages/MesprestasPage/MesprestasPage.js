bef58995cd8966e92dd9bd66c6a02e567cb7ecff
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2022-12-12 09:40:24 +0100

    added email to test at Prestas

--- web/src/pages/MesprestasPage/MesprestasPage.js
+++ web/src/pages/MesprestasPage/MesprestasPage.js
@@ -7,7 +7,7 @@ const MesprestasPage = () => {
       <MetaTags title="Mesprestas" description="Mesprestas page" />
 
       <div className="flex h-full w-full flex-col items-center justify-evenly bg-white p-2">
-        <div className="text-center text-yellow-400">
+        <div className="pb-4 text-center text-yellow-400">
           <p className="-rotate-12 text-2xl font-bold">
             Être bien accompagné pour
           </p>
@@ -17,7 +17,7 @@ const MesprestasPage = () => {
         </div>
 
         <div>
-          <div className="pb-10">
+          <div className="pb-6">
             <p className="pb-2 font-semibold text-green-500">
               Activ’Conseils intervient auprès des demandeurs d’emploi :
             </p>
@@ -47,7 +47,7 @@ const MesprestasPage = () => {
             </ul>
           </div>
 
-          <div>
+          <div className="pb-6">
             <p className="pb-2 font-semibold text-green-500">
               Activ’Conseils intervient auprès des scolaires :
             </p>
@@ -64,6 +64,18 @@ const MesprestasPage = () => {
                 personnel)
               </li>
             </ul>
+          </div>
+          <div>
+            <p className="pb-2 font-semibold text-green-500">
+              Nous, contactez :{' '}
+            </p>
+            {/* <p className="">Email : activconseils.sasu@gmail.com </p> */}
+            <a href="mailto:philocles.jeanmarie@gmail.com">
+            <div className="flex gap-2">
+              <p>Email : </p> <p className="text-blue-600 underline">activconseils.sasu@gmail.com</p>
+            </div>
+
+            </a>
           </div>
         </div>
       </div>
