1e4b97314481f1a7c8485d7c76ab55c8d27e5fde
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2022-12-18 15:58:22 +0100

    adj logo switch

--- web/src/layouts/WebsiteLayout/WebsiteLayout.js
+++ web/src/layouts/WebsiteLayout/WebsiteLayout.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from 'react'
 
 import { Link, navigate, routes } from '@redwoodjs/router'
 
-import logoactivconseils from './logoactivconseils.jpg'
+import logoactivconseils_w from './logoactivconseils_w.jpg'
+
+import logoactivconseils_b from './logoactivconseils_b.jpg'
+
+import threedyingyang from './yin-yang.jpg'
 
 const WebsiteLayout = ({ children }) => {
   const [theme, setTheme] = useState(null)
 
   useEffect(() => {
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       setTheme('dark')
     } else {
       setTheme('light')
@@ -30,28 +34,46 @@ const WebsiteLayout = ({ children }) => {
   return (
     <>
       <div className="flex h-screen w-screen flex-col items-center justify-center">
-        <header className="flex w-screen flex-row items-center justify-between bg-black pt-20 pb-2 pl-2 pr-4 text-white">
+        <header className="flex w-screen flex-row items-center justify-between pt-20 pb-2 pl-2 pr-4 text-white dark:bg-black">
           <Link to={routes.home()}>
             <div>
-              <img
-                className="rounded-full"
-                src={logoactivconseils}
-                // src="https://unsplash.com/photos/PonPBrdbiyg"
-                alt="logo pic"
-                width="70"
-                height="70"
-              />
+              {theme === 'dark' ? (
+                <img
+                  className=""
+                  src={logoactivconseils_b}
+                  // src="https://unsplash.com/photos/PonPBrdbiyg"
+                  alt="logo pic"
+                  width="100"
+                  height="50"
+                />
+              ) : (
+                <img
+                  className="rounded-full"
+                  src={logoactivconseils_w}
+                  // src="https://unsplash.com/photos/PonPBrdbiyg"
+                  alt="logo pic"
+                  width="70"
+                  height="70"
+                />
+              )}
             </div>
           </Link>
 
           <button
-            className='bg-white text-black font-bold p-1 rounded-3xl'
+            className="bg-white p-1 font-bold text-black"
             onClick={handleThemeSwitch}
           >
-            DM
+            <img
+              className="rounded-full"
+              src={threedyingyang}
+              // src="https://unsplash.com/photos/PonPBrdbiyg"
+              alt="logo pic"
+              width="50"
+              height="50"
+            />
           </button>
 
-          <ul className="flex flex-row items-end justify-end gap-4 text-xs text-yellow-400">
+          <ul className="flex flex-row items-end justify-end gap-4 text-xs text-black dark:text-yellow-400">
             <li
             // className='solid border border-green-500'
             >
@@ -73,14 +95,14 @@ const WebsiteLayout = ({ children }) => {
         </main>
 
         <div className="flex w-screen flex-col">
-          <footer className="w-screen bg-black pb- pt-10 text-yellow-400">
+          <footer className="pb- w-screen pt-10 text-black dark:bg-black dark:text-yellow-400">
             <ul>
               <li className="text-center">
                 Espace pour le texte pour de bas de page
               </li>
             </ul>
           </footer>
-          <div className="h-10 w-full bg-black">
+          <div className="h-10 w-full text-black dark:bg-black">
             This space needs to disappear
           </div>
         </div>
